9295d624c564de082e72cef4959930c4a1b5bfef
frdrckaman <frdrckaman@gmail.com>
2021-12-13 17:16:41 +0300

    login & register form

---
+++ src/public/Register.vue
@@ -0,0 +1,82 @@
+<template>
+    <main class="form-signin">
+        <form>
+            <h1 class="h3 mb-3 fw-normal">Please Sign UP</h1>
+
+            <div class="form-floating">
+                <input type="text" class="form-control" id="first_name" placeholder="First Name">
+                <label for="first_name">First Name</label>
+            </div>
+
+            <div class="form-floating">
+                <input type="text" class="form-control" id="last_name" placeholder="Last Name">
+                <label for="last_name">Last Name</label>
+            </div>
+
+            <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+            </div>
+
+            <div class="form-floating">
+                <input type="password" class="form-control" id="password" placeholder="Password">
+                <label for="password">Password</label>
+            </div>
+
+            <div class="form-floating">
+                <input type="password" class="form-control" id="passwordConfirm" placeholder="Password Confirm">
+                <label for="passwordConfirm">Password Confirm</label>
+            </div>
+
+            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign Up</button>
+        </form>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: "Register"
+    }
+</script>
+
+<style scoped>
+    html,
+    body {
+        height: 100%;
+    }
+
+    body {
+        display: flex;
+        align-items: center;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+    }
+
+    .form-signin {
+        width: 100%;
+        max-width: 330px;
+        padding: 15px;
+        margin: auto;
+    }
+
+    .form-signin .checkbox {
+        font-weight: 400;
+    }
+
+    .form-signin .form-floating:focus-within {
+        z-index: 2;
+    }
+
+    .form-signin input[type="email"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+
+    .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+    }
+</style>
